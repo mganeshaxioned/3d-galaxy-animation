e40d3320bf902c47c73b3e9b760aad3c4661abc8
mganeshprdxn <ganeshm@prdxn.com>
2021-12-08 17:27:49 +0530

    cusor changes added

--- src/script.js
+++ src/script.js
@@ -33,10 +33,10 @@ parameters.radius = 5
 parameters.branches = 8
 parameters.spin = 1
 parameters.randomness = 0.3
-parameters.randomnessPower = 4
+parameters.randomnessPower = 5
 parameters.stars = 100000
 parameters.starColor = '#37182a'
-parameters.insideColor = '#15286B'
+parameters.insideColor = '#627fd9'
 parameters.outsideColor = '#bbaeed'
 
 gui.add(parameters, 'count').min(100).max(100000).step(100).onChange(generateGalaxy).name('stars in galaxy')
@@ -262,3 +262,59 @@ const tick = () =>
 }
 
 tick()
+
+var mouseCursor = document.querySelector('.cursor')
+var mouseCursorImg = document.querySelector('.cursor img')
+var avatarsLi = document.querySelectorAll('.avatars-menu li')
+var avatarsWindow = document.querySelector('.avatars-window')
+
+let selectedAvatarSrc
+
+if (sessionStorage.getItem('selectedAvatar')) {
+    selectedAvatarSrc = sessionStorage.getItem('selectedAvatar')
+    mouseCursorImg.src = selectedAvatarSrc
+    avatarsWindow.classList.add("display-none")
+    document.querySelector("html").classList.add("cursor-none")
+}
+
+window.addEventListener('mousemove', cursor)
+
+avatarsLi.forEach(avtar => {
+    avtar.addEventListener('click', selectAvatar)
+})
+
+if (document.body.classList.contains("home")) {
+    document.querySelector(".custom-space").addEventListener("mouseenter", updateAvatar)
+    document.querySelector(".custom-space").addEventListener("mouseleave", defaultAvatar)
+}    
+
+function updateAvatar(e) {
+    if (!selectedAvatarSrc) { return false}
+    mouseCursorImg.src = "avatar2.png"
+}
+
+function defaultAvatar(e) {
+    if (!selectedAvatarSrc) { return false}
+    mouseCursorImg.src = selectedAvatarSrc
+}
+
+function selectAvatar(e) {
+    if (e.target.tagName === "LI") {
+        return false
+    }
+    selectedAvatarSrc = e.target.src
+    sessionStorage.setItem('selectedAvatar', selectedAvatarSrc)
+    mouseCursorImg.src = selectedAvatarSrc
+    mouseCursor.classList.remove("display-none")
+    avatarsWindow.classList.add("display-none")
+    document.querySelector("html").classList.add("cursor-none")
+}
+
+function cursor(e) {
+    mouseCursor.style.top = e.pageY + 'px';
+    mouseCursor.style.left = e.pageX + 'px';
+    if (sessionStorage.getItem('selectedAvatar')) {
+        mouseCursor.classList.remove("display-none")
+    }
+    // mouseCursor.setAttribute("style", "top: " + e.pageY + "px; left: " + e.pageX + "px;")
+}
